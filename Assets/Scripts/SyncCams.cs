20d53d173a5efd629fe4a7c138054d5a82e3050a
Nick Fox-Gieg <nick@fox-gieg.com>
2018-07-02 03:30:02 -0400

    first

---
+++ Assets/Scripts/SyncCams.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncCams : MonoBehaviour {
+
+    public Camera sourceCam;
+    public Camera[] destCam;
+
+	private void Start () {
+		foreach (Camera cam in destCam) {
+            cam.allowHDR = sourceCam.allowHDR;
+            cam.allowMSAA = sourceCam.allowMSAA;
+            cam.useOcclusionCulling = sourceCam.useOcclusionCulling;
+            cam.allowDynamicResolution = sourceCam.allowDynamicResolution;
+            cam.nearClipPlane = sourceCam.nearClipPlane;
+            cam.farClipPlane = sourceCam.farClipPlane;
+        }
+	}
+
+}
